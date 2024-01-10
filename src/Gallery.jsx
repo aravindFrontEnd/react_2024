b87ae9da268b99b6bf189fcb8c70a49d1826e4d5
Aravind Ganesh <91867114+Epsilon-Aravind@users.noreply.github.com>
2024-01-10 12:56:49 +0530

    added loading gif & pagination

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,25 +1,39 @@
+import { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import {useGlobalContext} from './context';
+import { useGlobalContext } from './context';
 import axios from 'axios';
-const URL = `https://api.unsplash.com/search/photos?client_id=${
-  import.meta.env.VITE_API_KEY
-}`;
+
+const IMAGES_PER_PAGE = 6;
+
+
+
+
 
 const Gallery = () => {
-  const {searchTerm}= useGlobalContext()
+
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const { searchTerm } = useGlobalContext();
+
+  const URL = `https://api.unsplash.com/search/photos?client_id=${
+    import.meta.env.VITE_API_KEY
+  }&page=${page}&per_page=${IMAGES_PER_PAGE}`;
+
+
+
   const response = useQuery({
-    queryKey: ['images', searchTerm],
+    queryKey: ['images', searchTerm,page],
     queryFn: async () => {
       const result = await axios.get(`${URL}&query=${searchTerm}`);
-
+    setTotalPages(result.data.total_pages);
       return result.data;
     },
   });
 
   if (response.isLoading) {
     return (
-      <section className='image-container'>
-        <h4>Loading..</h4>
+      <section className='image-container Loading'>
+        <img src="./loading.gif" alt="loading" />
       </section>
     );
   }
@@ -55,6 +69,14 @@ const Gallery = () => {
           ></img>
         );
       })}
+      <div className='buttons-search'>
+        {page > 1 && (
+          <button  className='prev-btn' onClick={() => setPage(page - 1)}>Previous</button>
+        )}
+        {page < totalPages && (
+          <button  className='next-btn' onClick={() => setPage(page + 1)}>Next</button>
+        )}
+      </div>
     </section>
   );
 };
